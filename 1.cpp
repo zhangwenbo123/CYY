dc63ebc19d879a675df7797a74b79d4e460985bb
zhangwenbo123 <2621494105@qq.com>
2017-09-20 00:01:18 +0800

    作业2

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+void main()
+{
+    int a;
+	cout<<"Please input a:";
+	cin>>a;
+	if(a<10)
+		cout<<a<<"<10"<<endl;
+	else
+		if(10<=a<=99)
+		cout<<"10<="<<a<<"<=99"<<endl;
+	else
+		if(100<=a<=999)
+		cout<<"100<="<<a<<"<=999"<<endl;
+	else
+	    if(a>=1000)
+		cout<<a<<">=1000"<<endl;
+}
