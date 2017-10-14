41e730399a50799e2d5914df59e57561bfb888b5
zhangwenbo123 <31693738+zhangwenbo123@users.noreply.github.com>
2017-10-15 00:12:36 +0800

    作业4_5

---
+++ 45.cpp
@@ -0,0 +1,31 @@
+#include"iostream.h"
+void main()
+{
+	int m,n;
+	for(m=1;m<=99;m++)
+	{
+		n=m*m;
+		if(m<=3)
+		{cout;}
+		else if
+		(m<=9,n%10==m)
+		{cout<<" "<<m<<endl;}
+		else if
+			(m<=32,n%100==m)
+		{cout<<" "<<m<<endl;}
+		else if(m<=99,n%1000==m)
+		{cout<<" "<<m<<endl;}
+		else
+			
+
+
+
+	
+	
+	
+	
+	
+	
+	;
+	}
+}     
